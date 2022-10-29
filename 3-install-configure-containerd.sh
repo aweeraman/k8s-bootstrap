f3e55d50dea9e75420e61090ff7850930157d3a2
Anuradha Weeraman <anuradha@debian.org>
2022-10-29 23:09:34 +0530

    Updated container configuration script

--- 3-install-configure-containerd.sh
+++ 3-install-configure-containerd.sh
@@ -8,16 +8,16 @@ echo "Configuring containerd and setting the cgroup driver to systemd..."
 containerd config default | tee /etc/containerd/config.toml
 
 patch /etc/containerd/config.toml --ignore-whitespace <<EOF
---- /a  2022-10-29 22:51:55.971059756 +0530
-+++ /b   2022-10-29 22:51:34.635143532 +0530
+--- /tmp/config.toml.default    2022-10-29 23:05:14.903922789 +0530
++++ /tmp/config.toml.updated    2022-10-29 23:06:02.419736222 +0530
 @@ -94,6 +94,7 @@
            privileged_without_host_devices = false
-	              base_runtime_spec = ""
-		                 [plugins."io.containerd.grpc.v1.cri".containerd.runtimes.runc.options]
-				 +            SystemdCgroup = true
-				      [plugins."io.containerd.grpc.v1.cri".cni]
-				             bin_dir = "/opt/cni/bin"
-					            conf_dir = "/etc/cni/net.d"
+           base_runtime_spec = ""
+           [plugins."io.containerd.grpc.v1.cri".containerd.runtimes.runc.options]
++            SystemdCgroup = true
+     [plugins."io.containerd.grpc.v1.cri".cni]
+       bin_dir = "/opt/cni/bin"
+       conf_dir = "/etc/cni/net.d"
 EOF
 
 echo "Restarting and enabling containerd..."
